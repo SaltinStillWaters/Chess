e49aa1aca96245d0b649e76d049f8bc845502321
Ralph Clarence C. Bello <82042428+RalphClarence1126@users.noreply.github.com>
2023-12-14 12:57:15 +0800

    Mouse Listener updates 2

--- src/main/java/Control/ClickEvent.java
+++ src/main/java/Control/ClickEvent.java
@@ -18,17 +18,25 @@ public class ClickEvent extends MouseAdapter implements MouseListener{
             TilePanel clickedTilePanel = (TilePanel) source;
             Model_Main tilesingleton = Model_Main.getInstance();
             
+            
             //check if the currTile of the singleton is not null
             if(tilesingleton.getCurrTileClicked() == null){
                 
                 tilesingleton.setCurrTileClicked(clickedTilePanel);
+                boolean turn = tilesingleton.getTurn();
+                boolean color = tilesingleton.getCurrTileClicked().getTile().getChessPiece().isWhite;
                 System.out.println("Current Tile set");
                 
-                if(tilesingleton.getCurrTileClicked().getPieceLabel() == null){
+                
+                //if the current tile does not have a piece the action or if the color of the piece in not the same as the turn in the singleton the action is cancelled
+                if(tilesingleton.getCurrTileClicked().getPieceLabel() == null || color != turn){
                    tilesingleton.setCurrTileClicked(null);
                    System.out.println("Action Cancelled");
                 }
+                
+                
             } else{
+                //Set the Tile Panel as destination if there is a current tile in the singleton
                 tilesingleton.setDestTileClicked(clickedTilePanel);
                 System.out.println("Destination Tile set");
             }
@@ -39,18 +47,24 @@ public class ClickEvent extends MouseAdapter implements MouseListener{
             if (tilesingleton.getDestTileClicked()!= null && tilesingleton.getDestTileClicked() != tilesingleton.getCurrTileClicked()){
                 // Get the PieceLabel from the current TilePanel
                 PieceLabel clickedPiece = tilesingleton.getCurrTileClicked().getPieceLabel();
-                if(clickedPiece != null){
+                boolean validMove = tilesingleton.getCurrTileClicked().getTile().getChessPiece().checkMove(tilesingleton.getCurrTileClicked().getTile().getCoordinate(), tilesingleton.getDestTileClicked().getTile().getCoordinate());
+                if(clickedPiece != null && validMove == true ){
                    tilesingleton.getCurrTileClicked().removePieceLabel();
                    tilesingleton.getDestTileClicked().setPieceLabel(clickedPiece);
                    // Reset the singleton's current and destination tiles after the move
                    System.out.println("Piece moved");
+                   tilesingleton.setTurn(!tilesingleton.getCurrTileClicked().getTile().getChessPiece().isWhite); //change turn
                    tilesingleton.setCurrTileClicked(null);
                    tilesingleton.setDestTileClicked(null);
+                } else {
+                   tilesingleton.setCurrTileClicked(null);
+                   tilesingleton.setDestTileClicked(null);
+                    System.out.println("Invalid move (Move not recognized)");
                 }
             } else if (tilesingleton.getDestTileClicked() == tilesingleton.getCurrTileClicked()){
                 tilesingleton.setCurrTileClicked(null);
                 tilesingleton.setDestTileClicked(null);
-                System.out.println("Invalid move");
+                System.out.println("Invalid move (Same Tile)");
             }
         }
         
--- src/main/java/Model/Model_Main.java
+++ src/main/java/Model/Model_Main.java
@@ -5,11 +5,12 @@ public class Model_Main {
     private static Model_Main instance;
     private TilePanel currTile;
     private TilePanel destTile;
+    private boolean isWhiteTurn = true;
+    
     
     private Model_Main(){
         
     }
-    
     
     public void setCurrTileClicked(TilePanel currTile){
         this.currTile = currTile;
@@ -28,6 +29,19 @@ public class Model_Main {
         return this.destTile;
     }
     
+    public void setTurn(boolean isWhiteTurn){
+        this.isWhiteTurn = isWhiteTurn;
+        if(this.isWhiteTurn == true){
+            System.out.println("White turn");
+        } else{
+            System.out.println("Black turn");
+        }
+    }
+    
+    public boolean getTurn(){
+        return this.isWhiteTurn;
+    }
+    
     
     public static Model_Main getInstance(){
         if (instance == null)
@@ -36,5 +50,7 @@ public class Model_Main {
         }
         return instance;
     }
+    
+    
     
 }
